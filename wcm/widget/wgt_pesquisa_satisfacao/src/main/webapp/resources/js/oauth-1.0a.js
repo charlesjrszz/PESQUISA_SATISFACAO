80d0ea0c459211cc0e2740f861e4234b259fa511
charles junior de souza carvalho <charlesjuniordesouza2007@gmail.com>
2026-01-21 10:25:33 -0300

    teste 2

--- wcm/widget/wgt_pesquisa_satisfacao/src/main/webapp/resources/js/oauth-1.0a.js
+++ wcm/widget/wgt_pesquisa_satisfacao/src/main/webapp/resources/js/oauth-1.0a.js
@@ -1,7 +1,7 @@
 if (typeof(module) !== 'undefined' && typeof(exports) !== 'undefined') {
     module.exports = OAuth;
 }
- 
+
 /**
  * Constructor
  * @param {Object} opts consumer key and secret
@@ -10,44 +10,44 @@ function OAuth(opts) {
     if(!(this instanceof OAuth)) {
         return new OAuth(opts);
     }
- 
+
     if(!opts) {
         opts = {};
     }
- 
+
     if(!opts.consumer) {
         throw new Error('consumer option is required');
     }
- 
+
     this.consumer            = opts.consumer;
     this.nonce_length        = opts.nonce_length || 32;
     this.version             = opts.version || '1.0';
     this.parameter_seperator = opts.parameter_seperator || ', ';
     this.realm               = opts.realm;
- 
+
     if(typeof opts.last_ampersand === 'undefined') {
         this.last_ampersand = true;
     } else {
         this.last_ampersand = opts.last_ampersand;
     }
- 
+
     // default signature_method is 'PLAINTEXT'
     this.signature_method = opts.signature_method || 'PLAINTEXT';
- 
+
     if(this.signature_method == 'PLAINTEXT' && !opts.hash_function) {
         opts.hash_function = function(base_string, key) {
             return key;
         }
     }
- 
+
     if(!opts.hash_function) {
         throw new Error('hash_function option is required');
     }
- 
+
     this.hash_function = opts.hash_function;
     this.body_hash_function = opts.body_hash_function || this.hash_function;
 }
- 
+
 /**
  * OAuth request authorize
  * @param  {Object} request data
@@ -67,28 +67,28 @@ OAuth.prototype.authorize = function(request, token) {
         oauth_timestamp: this.getTimeStamp(),
         oauth_version: this.version
     };
- 
+
     if(!token) {
         token = {};
     }
- 
+
     if(token.key !== undefined) {
         oauth_data.oauth_token = token.key;
     }
- 
+
     if(!request.data) {
         request.data = {};
     }
- 
+
     if(request.includeBodyHash) {
       oauth_data.oauth_body_hash = this.getBodyHash(request, token.secret)
     }
- 
+
     oauth_data.oauth_signature = this.getSignature(request, token.secret, oauth_data);
- 
+
     return oauth_data;
 };
- 
+
 /**
  * Create a OAuth Signature
  * @param  {Object} request data
@@ -99,21 +99,21 @@ OAuth.prototype.authorize = function(request, token) {
 OAuth.prototype.getSignature = function(request, token_secret, oauth_data) {
     return this.hash_function(this.getBaseString(request, oauth_data), this.getSigningKey(token_secret));
 };
- 
+
 /**
  * Create a OAuth Body Hash
  * @param {Object} request data
  */
 OAuth.prototype.getBodyHash = function(request, token_secret) {
   var body = typeof request.data === 'string' ? request.data : JSON.stringify(request.data)
- 
+
   if (!this.body_hash_function) {
     throw new Error('body_hash_function option is required');
   }
- 
+
   return this.body_hash_function(body, this.getSigningKey(token_secret))
 };
- 
+
 /**
  * Base String = Method + Base Url + ParameterString
  * @param  {Object} request data
@@ -123,7 +123,7 @@ OAuth.prototype.getBodyHash = function(request, token_secret) {
 OAuth.prototype.getBaseString = function(request, oauth_data) {
     return request.method.toUpperCase() + '&' + this.percentEncode(this.getBaseUrl(request.url)) + '&' + this.percentEncode(this.getParameterString(request, oauth_data));
 };
- 
+
 /**
  * Get data from url
  * -> merge with oauth data
@@ -141,9 +141,9 @@ OAuth.prototype.getParameterString = function(request, oauth_data) {
     } else {
         base_string_data = this.sortObject(this.percentEncodeData(this.mergeObject(oauth_data, this.mergeObject(request.data, this.deParamUrl(request.url)))));
     }
- 
+
     var data_str = '';
- 
+
     //base_string_data to string
     for(var i = 0; i < base_string_data.length; i++) {
         var key = base_string_data[i].key;
@@ -153,7 +153,7 @@ OAuth.prototype.getParameterString = function(request, oauth_data) {
         if (value && Array.isArray(value)){
           // sort the array first
           value.sort();
- 
+
           var valString = "";
           // serialize all values for this key: e.g. formkey=formvalue1&formkey=formvalue2
           value.forEach((function(item, i){
@@ -167,12 +167,12 @@ OAuth.prototype.getParameterString = function(request, oauth_data) {
           data_str += key + '=' + value + '&';
         }
     }
- 
+
     //remove the last character
     data_str = data_str.substr(0, data_str.length - 1);
     return data_str;
 };
- 
+
 /**
  * Create a Signing Key
  * @param  {String} token_secret Secret Token
@@ -180,14 +180,14 @@ OAuth.prototype.getParameterString = function(request, oauth_data) {
  */
 OAuth.prototype.getSigningKey = function(token_secret) {
     token_secret = token_secret || '';
- 
+
     if(!this.last_ampersand && !token_secret) {
         return this.percentEncode(this.consumer.secret);
     }
- 
+
     return this.percentEncode(this.consumer.secret) + '&' + this.percentEncode(token_secret);
 };
- 
+
 /**
  * Get base url
  * @param  {String} url
@@ -196,7 +196,7 @@ OAuth.prototype.getSigningKey = function(token_secret) {
 OAuth.prototype.getBaseUrl = function(url) {
     return url.split('?')[0];
 };
- 
+
 /**
  * Get data from String
  * @param  {String} string
@@ -205,13 +205,13 @@ OAuth.prototype.getBaseUrl = function(url) {
 OAuth.prototype.deParam = function(string) {
     var arr = string.split('&');
     var data = {};
- 
+
     for(var i = 0; i < arr.length; i++) {
         var item = arr[i].split('=');
- 
+
         // '' value
         item[1] = item[1] || '';
- 
+
         // check if the key already exists
         // this can occur if the QS part of the url contains duplicate keys like this: ?formkey=formvalue1&formkey=formvalue2
         if (data[item[0]]){
@@ -227,10 +227,10 @@ OAuth.prototype.deParam = function(string) {
           data[item[0]] = decodeURIComponent(item[1]);
         }
     }
- 
+
     return data;
 };
- 
+
 /**
  * Get data from url
  * @param  {String} url
@@ -238,13 +238,13 @@ OAuth.prototype.deParam = function(string) {
  */
 OAuth.prototype.deParamUrl = function(url) {
     var tmp = url.split('?');
- 
+
     if (tmp.length === 1)
         return {};
- 
+
     return this.deParam(tmp[1]);
 };
- 
+
 /**
  * Percent Encode
  * @param  {String} str
@@ -258,7 +258,7 @@ OAuth.prototype.percentEncode = function(str) {
         .replace(/\(/g, "%28")
         .replace(/\)/g, "%29");
 };
- 
+
 /**
  * Percent Encode Object
  * @param  {Object} data
@@ -266,7 +266,7 @@ OAuth.prototype.percentEncode = function(str) {
  */
 OAuth.prototype.percentEncodeData = function(data) {
     var result = {};
- 
+
     for(var key in data) {
         var value = data[key];
         // check if the value is an array
@@ -282,10 +282,10 @@ OAuth.prototype.percentEncodeData = function(data) {
         }
         result[this.percentEncode(key)] = value;
     }
- 
+
     return result;
 };
- 
+
 /**
  * Get OAuth data as Header
  * @param  {Object} oauth_data
@@ -293,25 +293,25 @@ OAuth.prototype.percentEncodeData = function(data) {
  */
 OAuth.prototype.toHeader = function(oauth_data) {
     var sorted = this.sortObject(oauth_data);
- 
+
     var header_value = 'OAuth ';
- 
+
     if (this.realm) {
         header_value += 'realm="' + this.realm + '"' + this.parameter_seperator;
     }
- 
+
     for(var i = 0; i < sorted.length; i++) {
         if (sorted[i].key.indexOf('oauth_') !== 0)
             continue;
- 
+
         header_value += this.percentEncode(sorted[i].key) + '="' + this.percentEncode(sorted[i].value) + '"' + this.parameter_seperator;
     }
- 
+
     return {
         Authorization: header_value.substr(0, header_value.length - this.parameter_seperator.length) //cut the last chars
     };
 };
- 
+
 /**
  * Create a random word characters string with input length
  * @return {String} a random word characters string
@@ -319,14 +319,14 @@ OAuth.prototype.toHeader = function(oauth_data) {
 OAuth.prototype.getNonce = function() {
     var word_characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     var result = '';
- 
+
     for(var i = 0; i < this.nonce_length; i++) {
         result += word_characters[parseInt(Math.random() * word_characters.length, 10)];
     }
- 
+
     return result;
 };
- 
+
 /**
  * Get Current Unix TimeStamp
  * @return {Int} current unix timestamp
@@ -334,9 +334,9 @@ OAuth.prototype.getNonce = function() {
 OAuth.prototype.getTimeStamp = function() {
     return parseInt(new Date().getTime()/1000, 10);
 };
- 
+
 ////////////////////// HELPER FUNCTIONS //////////////////////
- 
+
 /**
  * Merge object
  * @param  {Object} obj1
@@ -346,14 +346,14 @@ OAuth.prototype.getTimeStamp = function() {
 OAuth.prototype.mergeObject = function(obj1, obj2) {
     obj1 = obj1 || {};
     obj2 = obj2 || {};
- 
+
     var merged_obj = obj1;
     for(var key in obj2) {
         merged_obj[key] = obj2[key];
     }
     return merged_obj;
 };
- 
+
 /**
  * Sort object by key
  * @param  {Object} data
@@ -362,9 +362,9 @@ OAuth.prototype.mergeObject = function(obj1, obj2) {
 OAuth.prototype.sortObject = function(data) {
     var keys = Object.keys(data);
     var result = [];
- 
+
     keys.sort();
- 
+
     for(var i = 0; i < keys.length; i++) {
         var key = keys[i];
         result.push({
@@ -372,7 +372,6 @@ OAuth.prototype.sortObject = function(data) {
             value: data[key],
         });
     }
- 
+
     return result;
 };
- 
--- wcm/widget/wgt_pesquisa_satisfacao/src/main/webapp/resources/js/wgt_pesquisa_satisfacao.js
+++ wcm/widget/wgt_pesquisa_satisfacao/src/main/webapp/resources/js/wgt_pesquisa_satisfacao.js
@@ -1,21 +1,140 @@
-var WgtPesquisaSatisfacao = SuperWidget.extend({
-    //variáveis da widget
-    variavelNumerica: null,
-    variavelCaracter: null,
+var oauth = '';
 
-    //método iniciado quando a widget é carregada
-    init: function() {
+// Tokens de Acesso
+var token = {
+    key: 'fa4d7b2e-f2e3-4f07-962b-2ec0187cdba9',
+    secret: '0e1a4d17-11c0-40b6-89e4-d33dda50ae46baa6a5ef-77df-4205-a848-666be908d4ef'
+};
+
+var loadingOverlay;
+var successModal;
+var errorModal;
+
+// Função auxiliar Hex -> Base64
+function hexToBase64(hexstring) {
+    return btoa(hexstring.match(/\w{2}/g).map(function(a) {
+        return String.fromCharCode(parseInt(a, 16));
+    }).join(""));
+}
+
+var WgtPesquisaSatisfacao = SuperWidget.extend({
+    init: function () {
+        var self = this;
+        loadingOverlay = $('#loading-overlay');
+        successModal = $('#success-modal');
+        errorModal = $('#error-modal');
+
+        // Configuração OAuth (SHA256)
+        oauth = OAuth({
+            consumer: {
+                'key': 'getTeste',
+                'secret': 'getTeste'
+            },
+            signature_method: 'HMAC-SHA256',
+            hash_function: function(base_string, key) {
+                var hashHex = sha256.hmac(key, base_string);
+                return hexToBase64(hashHex);
+            }
+        });
+
+        $('#success-modal-close').on('click', function () { esconderSucesso(); });
+        $('#error-modal-close').on('click', function () { esconderErro(); });
     },
-  
-    //BIND de eventos
+
     bindings: {
-        local: {
-            'execute': ['click_executeAction']
-        },
+        local: { 'enviar': ['click_enviarSolicitscao'] },
         global: {}
     },
- 
-    executeAction: function(htmlElement, event) {
-    }
 
+    // Removido 'async' para o Eclipse não reclamar
+    enviarSolicitscao: function (htmlElement, event) {
+        var self = this;
+        
+        // Validação
+        if ($("#avaliacao_valor").val() == "") {
+            alertError("Por favor, selecione uma nota.");
+            return false;
+        }
+
+        mostrarLoading();
+
+        // Substituído 'await' por '.then()' (Padrão compatível com Eclipse)
+        startProcesAsync().then(function(data) {
+            // SUCESSO
+            var numProtocolo = data.processInstanceId;
+
+            esconderLoading();
+
+            $('#success-modal h3').text('Sucesso!');
+            $('#success-modal p').html('Pesquisa enviada.<br>Protocolo: <strong>' + numProtocolo + '</strong>');
+            mostrarSucesso();
+
+            if($("#form")[0]) $("#form")[0].reset();
+            $("#avaliacao_valor").val("");
+            $(".rating-item").removeClass("selected");
+
+        }, function(error) {
+            // ERRO
+            console.error("Erro envio:", error);
+            esconderLoading();
+
+            var msgErro = "Erro ao enviar solicitação.";
+            if (error.responseJSON && error.responseJSON.message) {
+                msgErro = error.responseJSON.message;
+            } else if (error.responseText) {
+                 var tmp = document.createElement("DIV");
+                 tmp.innerHTML = error.responseText;
+                 msgErro = tmp.textContent || tmp.innerText || error.responseText;
+            }
+
+            $('#error-modal p').text(msgErro);
+            mostrarErro();
+        });
+    }
 });
+
+function startProcesAsync() {
+    var processId = "PESQUISA_SATISFACAO"; 
+    
+    var request_data = {
+        url: parent.WCMAPI.serverURL + '/process-management/api/v2/processes/' + processId + '/start',
+        method: 'POST'
+    };
+
+    var obj = {};
+    $("#form").serializeArray().forEach(function(field) {
+        obj[field.name] = field.value;
+    });
+    
+    obj["avaliacao_valor"] = $("#avaliacao_valor").val();
+
+    var payload = {
+        "targetState": 0,             
+        "subProcessTargetState": 0,   
+        "comment": "Iniciado via Widget Público",
+        "formFields": obj
+    };
+
+    var headers = oauth.toHeader(oauth.authorize({
+        url: request_data.url,
+        method: request_data.method,
+        data: {} 
+    }, token));
+
+    return $.ajax({
+        url: request_data.url,
+        type: request_data.method,
+        data: JSON.stringify(payload),
+        contentType: "application/json; charset=UTF-8",
+        headers: { "Authorization": headers.Authorization }
+    });
+}
+
+// Funções Visuais
+function alertError(msg) { FLUIGC.toast({ title: 'Atenção:', message: msg, type: 'warning' }); }
+function mostrarLoading() { if (loadingOverlay) loadingOverlay.removeClass('loading-hidden'); }
+function esconderLoading() { if (loadingOverlay) loadingOverlay.addClass('loading-hidden'); }
+function mostrarSucesso() { if (successModal) successModal.removeClass('modal-hidden'); }
+function mostrarErro() { if (errorModal) errorModal.removeClass('modal-hidden'); }
+function esconderSucesso() { if (successModal) successModal.addClass('modal-hidden'); }
+function esconderErro() { if (errorModal) errorModal.addClass('modal-hidden'); }
